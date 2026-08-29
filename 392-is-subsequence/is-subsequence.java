e42987cf7d32b50de2a4e89caa2209aba964993e
NeuralHack <kalendrak527@gmail.com>
2026-08-29 14:49:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (45.16%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+         
+        char[] arr1=s.toCharArray();
+        char [] arr2=t.toCharArray();
+
+        int n=arr1.length;
+        int m=arr2.length;
+        int small=Math.min(n,m);
+
+        int p1=0;
+        int p2=0;
+        int count=0;
+        while(p1<n&&p2<m){
+            if(arr1[p1]==arr2[p2]){
+                count++;
+                p1++;
+                p2++;
+            }
+            else 
+                p2++;
+        }
+
+       return p1 == n;
+    }
+        
+    }
